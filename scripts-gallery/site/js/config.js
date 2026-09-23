d528fa303a9c49b81a71ece631372ec7d1704d3c
Zenzuke <zenzuke@gmail.com>
2026-09-23 22:53:11 +0200

    Add scripts gallery source (fichas + plantilla)

---
+++ scripts-gallery/site/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * Ajustes del sitio. Todo lo que se puede cambiar sin tocar la lógica.
+ */
+export const CONFIG = {
+  catalogUrl: "scripts.json",
+  langStorageKey: "zen_scripts_lang",
+  fallbackLang: "en",
+  scrollDelayMs: 200,
+};
